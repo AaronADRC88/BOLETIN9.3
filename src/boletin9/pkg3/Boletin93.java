ea6a989e9600475caa031b14b2b095dd71791e04
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-11-13 12:02:04 +0100

    A VER PAZÓ

---
+++ src/boletin9/pkg3/Boletin93.java
@@ -0,0 +1,12 @@
+
+package boletin9.pkg3;
+
+public class Boletin93 {
+
+    public static void main(String[] args) {
+        Arearectangulo obx=new Arearectangulo();
+        obx.validarFloat();
+        obx.calcular();
+    }
+    
+}
